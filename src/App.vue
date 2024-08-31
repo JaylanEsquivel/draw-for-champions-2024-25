1c870d6b25eeb100bc7b69568e90836af97e2ba2
JaylanEsquivel <jaylan1997@hotmail.com>
2024-08-31 17:32:00 -0300

    feat: project structure

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+  <HelloWorld msg="Champions League 24/25" />
+
+</template>
+
+<style scoped>
+
+</style>
